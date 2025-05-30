3e5de443ed4bcadb3e4bbd3bc5cbd9109f6744be
Reider14 <haider14guilherme@gmail.com>
2025-05-30 22:34:53 -0100

    Uploading the the screen of Chat Messages completed

--- lib/Screens/Messages/MessagesScreen.dart
+++ lib/Screens/Messages/MessagesScreen.dart
@@ -4,6 +4,88 @@ import 'package:untitled6/wigdgets/uihelper.dart';
 
 class messagesScreen extends StatelessWidget {
   TextEditingController searchController = TextEditingController();
+  var arrContent = [
+    {"img": "Oval.png", "name": "joshua_l", "lastmsg": "Have a nice day, bro!"},
+    {
+      "img": "Oval (1).png",
+      "name": "karennne",
+      "lastmsg": "I heard this is a good movie, s…",
+    },
+    {
+      "img": "Oval (2).png",
+      "name": "martini_rond",
+      "lastmsg": "See you on the next meeting!",
+    },
+    {
+      "img": "Oval (3).png",
+      "name": "andrewww_",
+      "lastmsg": "Sounds good 😂😂😂",
+    },
+    {"img": "Oval.png", "name": "joshua_l", "lastmsg": "Have a nice day, bro!"},
+    {
+      "img": "Oval (1).png",
+      "name": "karennne",
+      "lastmsg": "I heard this is a good movie, s…",
+    },
+    {
+      "img": "Oval (2).png",
+      "name": "martini_rond",
+      "lastmsg": "See you on the next meeting!",
+    },
+    {
+      "img": "Oval (3).png",
+      "name": "andrewww_",
+      "lastmsg": "Sounds good 😂😂😂",
+    },
+    {"img": "Oval.png", "name": "joshua_l", "lastmsg": "Have a nice day, bro!"},
+    {
+      "img": "Oval (1).png",
+      "name": "karennne",
+      "lastmsg": "I heard this is a good movie, s…",
+    },
+    {
+      "img": "Oval (2).png",
+      "name": "martini_rond",
+      "lastmsg": "See you on the next meeting!",
+    },
+    {
+      "img": "Oval (3).png",
+      "name": "andrewww_",
+      "lastmsg": "Sounds good 😂😂😂",
+    },
+    {"img": "Oval.png", "name": "joshua_l", "lastmsg": "Have a nice day, bro!"},
+    {
+      "img": "Oval (1).png",
+      "name": "karennne",
+      "lastmsg": "I heard this is a good movie, s…",
+    },
+    {
+      "img": "Oval (2).png",
+      "name": "martini_rond",
+      "lastmsg": "See you on the next meeting!",
+    },
+    {
+      "img": "Oval (3).png",
+      "name": "andrewww_",
+      "lastmsg": "Sounds good 😂😂😂",
+    },
+    {"img": "Oval.png", "name": "joshua_l", "lastmsg": "Have a nice day, bro!"},
+    {
+      "img": "Oval (1).png",
+      "name": "karennne",
+      "lastmsg": "I heard this is a good movie, s…",
+    },
+    {
+      "img": "Oval (2).png",
+      "name": "martini_rond",
+      "lastmsg": "See you on the next meeting!",
+    },
+    {
+      "img": "Oval (3).png",
+      "name": "andrewww_",
+      "lastmsg": "Sounds good 😂😂😂",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,35 +110,72 @@ class messagesScreen extends StatelessWidget {
         ],
       ),
 
-      body: Column(children: [
-        Row(
-          children: [
-            SizedBox(width: 20,),
-            Container(
-              height: 40,
-              width: 372,
-              decoration: BoxDecoration(
-                color: Color(0xFF262626),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 3),
-                child: TextField(       // is like a placeholder
-                  controller: searchController,
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    hintStyle: TextStyle(fontWeight: FontWeight.normal, color: Color(0xFFE8E893)),
-                    prefixIcon: UiHelper.CustomImage(imgurl: "Search Icon.png"),
-                    border: InputBorder.none
-
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(width: 20),
+              Container(
+                height: 40,
+                width: 372,
+                decoration: BoxDecoration(
+                  color: Color(0xFF262626),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 3),
+                  child: TextField(
+                    // is like a placeholder
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xFFE8E893),
+                      ),
+                      prefixIcon: UiHelper.CustomImage(
+                        imgurl: "Search Icon.png",
+                      ),
+                      border: InputBorder.none,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        )
-      ],),
+            ],
+          ),
 
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                // renderizar um array de objetos
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage("assets/images/${arrContent[index]["img"].toString()}"),
+                  ),
+                   // rederize as imagens no array de objetos com a chave name por index
+                  title: Text(arrContent[index]["name"].toString(), style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),),
+                  subtitle: Text(arrContent[index]["lastmsg"].toString(), style: TextStyle(fontSize: 13, color: Colors.grey),),
+                  trailing: UiHelper.CustomImage(imgurl: "Picture.png"),
+                );
+              },
+              itemCount: arrContent.length // contar o tamanho do array
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: SizedBox(
+        height: 79,
+        width: double.infinity,
+        child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF121212)), child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          UiHelper.CustomImage(imgurl: "Icon (4).png"),
+          SizedBox(width: 5,),
+          Text("Camera", style: TextStyle(fontSize: 13, color: Color(0xFF3897F0)),)
+        ],)),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
